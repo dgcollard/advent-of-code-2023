f55e7e0f2c4e1828e3f16604699faf8aa8ddd58b
David Collard <dgcollard@fastmail.com>
2023-12-09 14:44:06 +0000

    day 9 solution

---
+++ src/day-9.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from "vitest";
+import { Input, parseInputFile, part1, part2 } from "./day-9";
+import { trim } from "./util";
+
+const map: Input = [
+  [0, 3, 6, 9, 12, 15],
+  [1, 3, 6, 10, 15, 21],
+  [10, 13, 16, 21, 30, 45],
+];
+
+test("parseInputFile", () => {
+  expect(
+    parseInputFile(trim`
+      0 3 6 9 12 15
+      1 3 6 10 15 21
+      10 13 16 21 30 45
+    `)
+  ).toEqual(map);
+});
+
+test("part1", () => {
+  expect(part1(map)).toBe(114);
+});
+
+test("part2", () => {
+  expect(part2(map)).toBe(2);
+});
